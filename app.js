dace9b75dcefbad6b299beb98fe09ac635b41fe5
jonseo <jhines@media-inc.com>
2014-07-23 11:58:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-(function () {
-    window.onload = function () {
+(function() {
+    window.onload = function() {
         var html = '',
             map = document.getElementById('map-canvas'),
-            latLng = new google.maps.LatLng(21.7679, 78.8718),
+            latLng = new google.maps.LatLng(47.30791606293087, -122.34500865000001),
             infowindow, users = [],
             markers = []
-            options = {
+        options = {
                 center: latLng,
-                zoom: 5,
+                zoom: 6,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 disableDefaultUI: true,
                 zoomControl: true,
@@ -15,18 +15,18 @@
                 scrollwheel: true,
             },
             map = new google.maps.Map(map, options);
-        google.maps.Map.prototype.clearMarkers = function () {
+        google.maps.Map.prototype.clearMarkers = function() {
             for (var i = 0; i < markers.length; i++) {
                 markers[i].setMap(null);
             }
             markers = new Array();
         };
 
-        $.getJSON("data.json", function (data) {
-            $.each(data, function (key, user) {
+        $.getJSON("data.json", function(data) {
+            $.each(data, function(key, user) {
                 users.push(user);
                 html += '<li class="row" data-id="' + key + '"><div class="col-md-4"><img src="' + (user.picture) + '"></div><div class="col-md-8"><span><a href="javascript:" class="m">' + (user.name) + '</a></span><br/><span>' + (user.team) + '</span><br/><span>' + (user.email) + '</span></div></li>';
-                setTimeout(function () {
+                setTimeout(function() {
                     draw(user);
                 }, 125 + (key * 125));
             });
@@ -34,19 +34,19 @@
 
         });
 
-        window.setTimeout(function () {
-            $(".m").on("click", function () {
+        window.setTimeout(function() {
+            $(".m").on("click", function() {
 
                 map.clearMarkers();
-               // $("#rp").prop("checked", true);
+                // $("#rp").prop("checked", true);
                 $('#search').val($.trim($(this).text())).trigger('keyup');
-            //    $("#rp").prop("checked", false);
+                //    $("#rp").prop("checked", false);
             });
 
 
         }, 125);
 
-        var draw = function (user) {
+        var draw = function(user) {
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(user.latitude, user.longitude),
                 map: map,
@@ -62,7 +62,7 @@
                 '<p><b>' + user.email + ' | ' + user.phone + '</b><br/>' + user.about + '</p>' +
                 '</div>' +
                 '</div>';
-            google.maps.event.addListener(marker, 'click', function () {
+            google.maps.event.addListener(marker, 'click', function() {
                 if (!infowindow) {
                     infowindow = new google.maps.InfoWindow();
                 }
@@ -72,21 +72,21 @@
 
         };
 
-        $('#search').delayKeyup(function (ele) {
-           // var isChecked = $("#rp").is(":checked");
+        $('#search').delayKeyup(function(ele) {
+            // var isChecked = $("#rp").is(":checked");
             //if (isChecked || $.trim($(ele).val()).length == 0)
             map.clearMarkers();
             var valThis = $(ele).val().toLowerCase();
-            $('#results>li').each(function (key, val) {
+            $('#results>li').each(function(key, val) {
                 var text = $(this).text().toLowerCase();
 
                 if (text.indexOf(valThis) >= 0) {
                     var _self = $(this);
                     _self.show();
                     //if (isChecked || $.trim($("#search").val()).length == 0) {
-                        //setTimeout(function () {
-                            draw(users[parseInt(_self.data("id"))]);
-                        //}, 125 + (key * 125));
+                    //setTimeout(function () {
+                    draw(users[parseInt(_self.data("id"))]);
+                    //}, 125 + (key * 125));
                     //}
                 } else {
                     $(this).hide();
@@ -113,11 +113,11 @@
 
 
     };
-   
-    (function ($) {
-        $.fn.delayKeyup = function (callback, ms) {
+    //http://code.azerti.net/javascript/jquery/delaykeyup.htm
+    (function($) {
+        $.fn.delayKeyup = function(callback, ms) {
             var timer = 0;
-            $(this).keyup(function () {
+            $(this).keyup(function() {
                 clearTimeout(timer);
                 timer = setTimeout(callback($(this)), ms);
             });
